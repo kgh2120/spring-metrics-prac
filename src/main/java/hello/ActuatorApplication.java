758eda546bdd12a27d216968fbbe375291e952d3
kyoohyun <kgh2120@gmail.com>
2023-10-10 17:40:25 +0900

    init pjt

---
+++ src/main/java/hello/ActuatorApplication.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication(scanBasePackages = "hello.controller")
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+    @Bean
+    public HttpTraceRepository httpExchangeRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
+
+}
